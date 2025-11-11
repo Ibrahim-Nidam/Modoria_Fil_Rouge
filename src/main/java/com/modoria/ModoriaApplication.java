fa74e4caf63d8209a3ade618f7850a9a6fc81c75
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-11 14:18:42 +0100

    chore: initial project structure with configuration and main application files

---
+++ src/main/java/com/modoria/ModoriaApplication.java
@@ -0,0 +1,13 @@
+package com.modoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModoriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modoria/ModoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
