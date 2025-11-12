ce7bf9d58dd67f643b9b0b20478bd8ac5dcdaa99
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-12 10:13:54 +0100

    MFR-22 Define handlers for RuntimeException, ValidationException, etc. #done

---
+++ src/main/java/com/modoria/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.modoria.exception;
+
+public class ResourceNotFoundException extends AppException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
