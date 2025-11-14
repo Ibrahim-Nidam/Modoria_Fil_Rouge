4768408fa554557110f4dcddf1a7c90011fbd843
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-14 10:46:42 +0100

    MFR-31 - MFR-32 Define Role entity #done

---
+++ src/main/java/com/modoria/model/User.java
@@ -0,0 +1,49 @@
+package com.modoria.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Full name is required")
+    @Column(name = "full_name", nullable = false, length = 100)
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @NotBlank(message = "Enabled field can't be null")
+    @Column(nullable = false)
+    private Boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+}
