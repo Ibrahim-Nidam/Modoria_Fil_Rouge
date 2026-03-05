db96ac4c04b87b3ed01ca19db03a4ad6726530fe
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-05 13:41:37 +0000

    feat(MFR-65): add uploadProductImage to ProductService interface

--- src/main/java/com/modoria/catalog/application/service/product/ProductService.java
+++ src/main/java/com/modoria/catalog/application/service/product/ProductService.java
@@ -2,6 +2,7 @@ package com.modoria.catalog.application.service.product;
 
 import com.modoria.catalog.application.dto.product.ProductRequestDTO;
 import com.modoria.catalog.application.dto.product.ProductResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,4 +16,6 @@ public interface ProductService {
     ProductResponseDTO updateProduct(Long id, ProductRequestDTO requestDTO);
 
     void deleteProduct(Long id);
+
+    ProductResponseDTO uploadProductImage(Long productId, MultipartFile file);
 }
