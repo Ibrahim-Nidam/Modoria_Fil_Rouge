1c4957aeaa80b14386fc51b03077cd8357278a90
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-05 12:23:49 +0000

    feat(MFR-54): add CategoryMapper using MapStruct

---
+++ src/main/java/com/modoria/catalog/application/mapper/category/CategoryMapper.java
@@ -0,0 +1,19 @@
+package com.modoria.catalog.application.mapper.category;
+
+import com.modoria.catalog.application.dto.category.CategoryRequestDTO;
+import com.modoria.catalog.application.dto.category.CategoryResponseDTO;
+import com.modoria.catalog.domain.model.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    Category toEntity(CategoryRequestDTO dto);
+
+    CategoryResponseDTO toResponseDTO(Category entity);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(CategoryRequestDTO dto, @MappingTarget Category entity);
+}
