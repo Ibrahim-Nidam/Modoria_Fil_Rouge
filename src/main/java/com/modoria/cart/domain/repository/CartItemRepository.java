f2c270c6b604d0cf83519593a7e9654b5cf2535a
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-05 15:30:21 +0000

    feat(MFR-75): add Cart and CartItem JPA repositories

---
+++ src/main/java/com/modoria/cart/domain/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.modoria.cart.domain.repository;
+
+import com.modoria.cart.domain.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
