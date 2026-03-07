ff1bbb4ce076305db4574efdf0e0f1d40436058e
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-07 07:13:06 +0000

    MFR-134: Add indexes to Product entity

--- src/main/java/com/modoria/catalog/domain/model/Product.java
+++ src/main/java/com/modoria/catalog/domain/model/Product.java
@@ -6,7 +6,10 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "products")
+@Table(name = "products", indexes = {
+        @Index(name = "idx_product_name", columnList = "name"),
+        @Index(name = "idx_product_season", columnList = "season")
+})
 @Getter
 @Setter
 @NoArgsConstructor
