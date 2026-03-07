fd57acb89ae7163f3c4d11dceb6f164d2296faf2
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-07 06:56:06 +0000

    MFR-129: Update ProductService interface to support Pageable

--- src/main/java/com/modoria/catalog/application/service/product/ProductService.java
+++ src/main/java/com/modoria/catalog/application/service/product/ProductService.java
@@ -2,6 +2,8 @@ package com.modoria.catalog.application.service.product;
 
 import com.modoria.catalog.application.dto.product.ProductRequestDTO;
 import com.modoria.catalog.application.dto.product.ProductResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -11,7 +13,7 @@ public interface ProductService {
 
     ProductResponseDTO getProductById(Long id);
 
-    List<ProductResponseDTO> getAllProducts();
+    Page<ProductResponseDTO> getAllProducts(Pageable pageable);
 
     ProductResponseDTO updateProduct(Long id, ProductRequestDTO requestDTO);
 
@@ -19,5 +21,5 @@ public interface ProductService {
 
     ProductResponseDTO uploadProductImage(Long productId, MultipartFile file);
 
-    List<ProductResponseDTO> getProductsBySeason(String season);
+    Page<ProductResponseDTO> getProductsBySeason(String season, Pageable pageable);
 }
