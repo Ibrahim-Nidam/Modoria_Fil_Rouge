0da841b42c6040a88073f1e656db4c02a3b77a4e
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-08 23:36:20 +0000

    feat(ui): add Table component

---
+++ Frontend/src/app/shared/ui/table/table.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+export interface TableColumn {
+  key: string;
+  label: string;
+}
+
+@Component({
+  selector: 'app-table',
+  imports: [],
+  templateUrl: './table.html',
+  styleUrl: './table.css',
+})
+export class Table {
+  @Input() columns: TableColumn[] = [];
+  @Input() data: any[] = [];
+  @Input() hoverable: boolean = false;
+  @Input() emptyMessage: string = 'No data available';
+
+  @Output() rowClick = new EventEmitter<any>();
+
+  onRowClick(row: any) {
+    if (this.hoverable) {
+      this.rowClick.emit(row);
+    }
+  }
+}
