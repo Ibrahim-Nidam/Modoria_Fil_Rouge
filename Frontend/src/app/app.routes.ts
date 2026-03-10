a47f70148cd1f78ea7834cf9490b2508ff1d9d6b
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-10 14:37:54 +0000

    feat: add/update app.routes.ts for MFR-161 auth integration

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./shared/layout/header/header').then(m => m.Header) // Temporary placeholder
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
