b3e146ea8a4e38a60f34b942f3a22e026057c6df
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-16 15:32:40 +0000

    chore(backend): update categoryservice

--- Backend/src/main/java/com/modoria/catalog/application/service/category/CategoryService.java
+++ Backend/src/main/java/com/modoria/catalog/application/service/category/CategoryService.java
@@ -2,6 +2,7 @@ package com.modoria.catalog.application.service.category;
 
 import com.modoria.catalog.application.dto.category.CategoryRequestDTO;
 import com.modoria.catalog.application.dto.category.CategoryResponseDTO;
+import com.modoria.catalog.domain.model.Season;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +12,7 @@ public interface CategoryService {
 
     CategoryResponseDTO getCategoryById(Long id);
 
-    Page<CategoryResponseDTO> getAllCategories(Pageable pageable);
+    Page<CategoryResponseDTO> getAllCategories(Pageable pageable, Season season);
 
     CategoryResponseDTO updateCategory(Long id, CategoryRequestDTO requestDTO);
 
