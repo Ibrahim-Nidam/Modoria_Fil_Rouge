b06df003e552fc2692d860a31a4956f5f50fc2f0
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-16 15:32:39 +0000

    refactor(backend): remove cartitemrepository

--- Backend/src/main/java/com/modoria/cart/domain/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.modoria.cart.domain.repository;
-
-import com.modoria.cart.domain.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
