31d719875c3285f1ae26bc689a6cc661873d14a0
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-03-21 17:05:34 +0000

    test(ci): provide jwt properties for test profile context load

--- Backend/src/test/java/com/modoria/ModoriaApplicationTests.java
+++ Backend/src/test/java/com/modoria/ModoriaApplicationTests.java
@@ -2,8 +2,10 @@ package com.modoria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ModoriaApplicationTests {
 
     @Test
